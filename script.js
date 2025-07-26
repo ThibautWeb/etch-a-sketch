baa6310bff10754438f543b94d38346e3ce5ded2
ThibautWeb <216151628+ThibautWeb@users.noreply.github.com>
2025-07-26 13:09:32 +0200

    Add grid reset button
    Also prompt for grid size

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 let container = document.querySelector(".container")
+createGrid(container, 16)
 
-for (let i = 1; i <= 16; i++) {
-  let gridRowDiv = document.createElement("div")
-  container.appendChild(gridRowDiv)
-  for (let j = 1; j <= 16; j++) {
-    let gridInnerDiv = document.createElement("div")
-    gridInnerDiv.addEventListener("mouseover", (e) => {
-      e.target.classList.add("hovered")
-    })
-    gridRowDiv.appendChild(gridInnerDiv)
-
+function createGrid(container, size) {
+  for (let i = 1; i <= size; i++) {
+    let gridRowDiv = document.createElement("div")
+    container.appendChild(gridRowDiv)
+    for (let j = 1; j <= size; j++) {
+      let gridInnerDiv = document.createElement("div")
+      gridInnerDiv.addEventListener("mouseenter", (e) => {
+        e.target.classList.add("hovered")
+      })
+      gridRowDiv.appendChild(gridInnerDiv)
+    }
   }
 }
 
+let button = document.querySelector("button")
+button.addEventListener("click", () => resetGrid(container))
 
+function resetGrid(container) {
+  console.log("hi")
+  let size = 0
+  while (size < 2 || size > 100) {
+    size = prompt("Enter a grid size between 2 and 100:")
+  }
+  container.querySelectorAll("div").forEach(el => el.remove())
+  createGrid(container, size)
+}
