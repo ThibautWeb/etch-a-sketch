6204a99f1647260c7f771fe4f7176db0cb2eda55
ThibautWeb <216151628+ThibautWeb@users.noreply.github.com>
2025-07-26 12:30:19 +0200

    Add painting feature
    Divs turn black when mouse over them

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let container = document.querySelector(".container")
 
-for (let i = 1; i <= 4; i++) {
+for (let i = 1; i <= 16; i++) {
   let gridRowDiv = document.createElement("div")
   container.appendChild(gridRowDiv)
-  for (let j = 1; j <= 4; j++) {
+  for (let j = 1; j <= 16; j++) {
     let gridInnerDiv = document.createElement("div")
+    gridInnerDiv.addEventListener("mouseover", (e) => {
+      e.target.classList.add("hovered")
+    })
     gridRowDiv.appendChild(gridInnerDiv)
+
   }
 }
+
+
